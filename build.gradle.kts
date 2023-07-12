610e6db72157892a2aefa174cbe4b1a04da12679
rgomez <rafael.gomez@code.seat>
2023-07-12 21:19:55 +0200

    REMOVE jib task

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,11 +87,6 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test)
 }
 
-jib {
-    container {
-        jvmFlags = listOf("-Xjsr305=strict", "-Xcontext-receivers")
-    }
-}
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
