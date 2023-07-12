8f40f76ef75932ec34e925fb0decef7b2cfaf7fb
rgomez <rafael.gomez@code.seat>
2023-07-12 22:50:39 +0200

    REMOVE Unwanted factory method

--- src/main/kotlin/com/codely/shared/error/ServerError.kt
+++ src/main/kotlin/com/codely/shared/error/ServerError.kt
@@ -2,7 +2,6 @@ package com.codely.shared.error
 
 data class ServerError(val code: String, val description: String? = null) {
     companion object {
-        fun of(code: String, description: String? = null) = ServerError(code, description)
         fun of(error: Pair<String, String>) = ServerError(error.first, error.second)
     }
 }
