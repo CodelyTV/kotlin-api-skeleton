70cbdca9bd571009025d253d95154f815e93d1ac
rgomez <rafael.gomez@code.seat>
2023-07-15 01:37:18 +0200

    ADD Disable Agenda Workflow

---
+++ src/main/kotlin/com/codely/agenda/primaryadapter/rest/disable/DisableAgendaController.kt
@@ -0,0 +1,65 @@
+package com.codely.agenda.primaryadapter.rest.disable
+
+import arrow.core.raise.Raise
+import arrow.core.raise.fold
+import arrow.core.raise.withError
+import com.codely.admin.application.validate.validate
+import com.codely.admin.domain.AccessKey
+import com.codely.admin.domain.AdminRepository
+import com.codely.admin.primaryadapter.rest.error.AdminServerErrors.INVALID_ACCESS_KEY
+import com.codely.agenda.application.disable.DisableAgendaCommand
+import com.codely.agenda.application.disable.DisableAgendaError
+import com.codely.agenda.application.disable.DisableAgendaError.AgendaNotFound
+import com.codely.agenda.application.disable.DisableAgendaError.ForbiddenAction
+import com.codely.agenda.application.disable.DisableAgendaError.InvalidUUID
+import com.codely.agenda.application.disable.DisableAgendaError.Unknown
+import com.codely.agenda.application.disable.handle
+import com.codely.agenda.domain.Agenda
+import com.codely.agenda.domain.AgendaRepository
+import com.codely.agenda.primaryadapter.rest.error.AgendaServerErrors.AGENDA_DOES_NOT_EXIST
+import com.codely.agenda.primaryadapter.rest.error.AgendaServerErrors.INVALID_IDENTIFIERS
+import com.codely.shared.error.ServerError
+import kotlinx.coroutines.runBlocking
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DisableAgendaController(
+    private val repository: AgendaRepository,
+    private val adminRepository: AdminRepository
+) {
+
+    @PostMapping("/agendas/{agendaId}")
+    fun disableAgenda(@PathVariable agendaId: String, @RequestParam accessKey: String): ResponseEntity<*> = runBlocking {
+        with(repository) {
+            with(adminRepository) {
+                fold(
+                    block = { executeCommand(accessKey, agendaId) },
+                    recover = { error -> error.toServerError() },
+                    transform = { agenda -> ResponseEntity.status(OK).body(agenda) }
+                )
+            }
+        }
+    }
+
+    context(AgendaRepository, AdminRepository, Raise<DisableAgendaError>)
+    private suspend fun executeCommand(accessKey: String, agendaId: String): Agenda {
+        withError(block = { validate(AccessKey(accessKey)) }, transform = { raise(ForbiddenAction) })
+        return handle(DisableAgendaCommand(agendaId))
+    }
+
+    private fun DisableAgendaError.toServerError() =
+        when (this) {
+            is AgendaNotFound -> ResponseEntity.status(NOT_FOUND).body(ServerError.of(AGENDA_DOES_NOT_EXIST))
+            is InvalidUUID -> ResponseEntity.status(BAD_REQUEST).body(ServerError.of(INVALID_IDENTIFIERS))
+            is ForbiddenAction -> ResponseEntity.status(FORBIDDEN).body(ServerError.of(INVALID_ACCESS_KEY))
+            is Unknown -> throw cause
+        }
+}
