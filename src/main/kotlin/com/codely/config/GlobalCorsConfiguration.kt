8d5edf275028faf687c73931c36d5ff2c82673b1
rgomez <rafael.gomez@code.seat>
2023-07-22 16:05:08 +0200

    TEST with CORS

--- src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
+++ src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
@@ -4,22 +4,31 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf()
-            .disable()
-            .cors()
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PATCH", "OPTIONS")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 
-        return http.build();
+    @Bean
+    fun springSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors()
+            .disable()
+            .build()
     }
 }
