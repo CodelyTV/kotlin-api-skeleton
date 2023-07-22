75d2d80628f6f8e84da9c24309085affa00f4e49
rgomez <rafael.gomez@code.seat>
2023-07-22 14:53:32 +0200

    ADD Spring Security

--- src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
+++ src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
@@ -1,20 +1,37 @@
 package com.codely.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Configuration
-@EnableWebMvc
-class GlobalCorsConfiguration : WebMvcConfigurer {
+//@Configuration
+//@EnableWebMvc
+//class GlobalCorsConfiguration : WebMvcConfigurer {
+//
+//    override fun addCorsMappings(registry: CorsRegistry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("*")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE")
+//            .allowedHeaders("*")
+//            .exposedHeaders("*")
+//            .allowCredentials(true).maxAge(3600)
+//    }
+//}
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .exposedHeaders("*")
-            .allowCredentials(true).maxAge(3600)
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors()
+
+        return http.build()
     }
 }
