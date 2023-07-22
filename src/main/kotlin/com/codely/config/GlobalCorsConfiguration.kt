03f22b0fed1cce2e86c51031a47167fcc08e3ab4
rgomez <rafael.gomez@code.seat>
2023-07-22 14:29:31 +0200

    ADD CORS Configuration v2

--- src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
+++ src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
@@ -5,9 +5,22 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class GlobalCorsConfiguration {
+@EnableWebMvc
+class GlobalCorsConfiguration : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowCredentials(true).maxAge(3600)
+    }
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
