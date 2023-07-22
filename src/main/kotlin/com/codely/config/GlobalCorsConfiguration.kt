d9889c23082b98ff108c5ad49a0329ccbe1c3663
rgomez <rafael.gomez@code.seat>
2023-07-22 14:32:31 +0200

    ADD CORS Configuration v3

--- src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
+++ src/main/kotlin/com/codely/config/GlobalCorsConfiguration.kt
@@ -1,16 +1,11 @@
 package com.codely.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
+
 class GlobalCorsConfiguration : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
@@ -20,19 +15,5 @@ class GlobalCorsConfiguration : WebMvcConfigurer {
             .allowedHeaders("*")
             .exposedHeaders("*")
             .allowCredentials(true).maxAge(3600)
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val corsConfig = CorsConfiguration()
-
-        corsConfig.allowedOrigins = listOf("*") // Replace * with your specific allowed origin(s) if needed
-        corsConfig.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        corsConfig.allowedHeaders = listOf("*")
-        corsConfig.allowCredentials = true
-
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", corsConfig)
-        return source
     }
 }
