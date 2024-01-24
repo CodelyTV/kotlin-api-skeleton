52064efd4c9d72ea1623724d0592fa64763102a7
rgomez <rafael.gomez@code.seat>
2024-01-24 21:12:28 +0100

    ADD Decode to club name

--- src/main/kotlin/com/codely/competition/ranking/infrastructure/rest/search/SearchRankingController.kt
+++ src/main/kotlin/com/codely/competition/ranking/infrastructure/rest/search/SearchRankingController.kt
@@ -8,12 +8,13 @@ import com.codely.shared.cors.BaseController
 import com.codely.shared.response.Response
 import com.codely.shared.response.withBody
 import kotlinx.coroutines.runBlocking
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.OK
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets.UTF_8
 
 @RestController
 class SearchRankingController(private val repository: LeagueRankingRepository): BaseController() {
@@ -21,9 +22,11 @@ class SearchRankingController(private val repository: LeagueRankingRepository): 
     @GetMapping("/rankings")
     fun search(@RequestParam league: String, @RequestParam club: String): Response<*> = runBlocking {
         with(repository) {
-            handle(SearchLeagueRankingQuery(league, club))
+            handle(SearchLeagueRankingQuery(league, club.decodedParameter()))
                 ?.let { ranking -> Response.status(OK).body(ranking.toDocument()) }
                 ?: Response.status(NOT_FOUND).withBody(LEAGUE_RANKING_DOES_NOT_EXIST)
         }
     }
+
+    private fun String.decodedParameter(): String = URLDecoder.decode(this, UTF_8)
 }
